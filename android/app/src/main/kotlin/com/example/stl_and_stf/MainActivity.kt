e270eae99bbc7ac0f3f3e6cedf980fac11cf1e55
Anchirinaah Hezekiah <ce-hanchirinaah3821@st.umat.edu.gh>
2024-12-03 00:17:32 +0000

    Stateless and Stateful widgets

---
+++ android/app/src/main/kotlin/com/example/stl_and_stf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stl_and_stf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Pages/counter_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  //variable
+  int _counter = 0;
+
+//method
+  void _incrementCounter() {
+    //setstate rebuilds the widget
+    setState(() {
+      _counter++;
+    });
+  }
+
+//UI(User Interface)
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('You have pushed the button this many times: '),
+            Text(
+              _counter.toString(),
+              style: TextStyle(fontSize: 40),
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text('Increment!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
